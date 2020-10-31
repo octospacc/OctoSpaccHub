6ceea9733b658351b095a458fb884cb643fa3ef3
octospacc <octo@tutamail.com>
2020-10-31 12:56:53 +0100

    css spacc and fix but not complete, more gifs added

--- public/assets/js/RandomGIF.js
+++ public/assets/js/RandomGIF.js
@@ -1,5 +1,5 @@
 function RandomGIF() {
-	gifid = Math.floor((Math.random() * 11)/2);
+	gifid = Math.floor((Math.random() * 25)/2);
 	gifurl = top.glob + gifid + ".gif";
 	gifcss = "url(" + gifurl + ")";
 	document.body.style.backgroundImage = gifcss;
