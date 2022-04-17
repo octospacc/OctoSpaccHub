81bbeb5bfb057cbfb716143464807c72f08ff9f5
Octo Andri <6083316-octospacc@users.noreply.gitlab.com>
2022-04-17 16:19:19 +0000

    Fix gif location

--- public/Assets/JS/RandomGIF.js
+++ public/Assets/JS/RandomGIF.js
@@ -1,6 +1,6 @@
 function RandomGIF() {
 	GifID = Math.floor((Math.random() * 25)/2);
-	GifURL = "Web-ThirdParty-Unknown/Assets/Media/Backgrounds/" + GifID + ".gif";
+	GifURL = "/Web-ThirdParty-Unknown/Assets/Media/Backgrounds/" + GifID + ".gif";
 	document.body.style.backgroundImage = "url(" + GifURL + ")";
 }
 
