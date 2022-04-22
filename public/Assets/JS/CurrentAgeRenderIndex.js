0293db6a8d30ca78c1db6d88c8bf4107e2b6b3c6
octospacc <octo@tutamail.com>
2022-04-23 00:19:31 +0200

    Birthday confetti

--- public/Assets/JS/CurrentAgeRenderIndex.js
+++ public/Assets/JS/CurrentAgeRenderIndex.js
@@ -1,9 +1,55 @@
 const CurrentAgeElem = document.getElementById("CurrentAge");
 const RefreshTime = 50;
 
-function UpdateHTML() {
-	CurrentAgeElem.innerHTML =
-		"Currently " + UnixToYears() + " years old..";
+const ConfettiSleep = 600;
+const ConfettiIter = 32;
+
+var ConfettiCalled = false;
+
+var SoundObject = new Audio( // Borrowed music: https://0101.bandcamp.com/track/fireworks
+	"https://matrix-client.matrix.org/_matrix/media/r0/download/matrix.org/bPhubIokhLQpTfMZhNsVucVO");
+SoundObject.muted = true;
+
+function Sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-setInterval(UpdateHTML, RefreshTime);
+async function Confetti() {
+	for (let i = 0; i < ConfettiIter; i++) {
+		const ConfettiCanvas = document.getElementById('ConfettiCanvas');
+		const JsConfetti = new JSConfetti({ConfettiCanvas});
+		JsConfetti.addConfetti({
+			emojis: [
+				'🌈', '⚡️', '💥',
+				'✨', '💫', '🌸',
+				'🏳️‍⚧️️', '🎉️', '🎊️',
+				'🏳️‍🌈️', '🎈️', '🎆️',
+				'💘️', '💝️', '💖️',
+				'💗️', '💞️', '❤️',
+				'💛️', '💚️', '💜️',
+				'🤎️', '🖤️', '🤍️',
+			]
+		});
+		JsConfetti.addConfetti();
+		await Sleep(ConfettiSleep);
+	}
+}
+
+
+function UpdateAge() {
+	let Years = UnixToYears();
+	CurrentAgeElem.innerHTML = "Currently " + Years + " years old..";
+	if ((Years % 1 == 0) && (!ConfettiCalled)) {
+		Confetti();
+		ConfettiCalled = true;
+	}
+	// music playing code
+}
+
+setInterval(UpdateAge, RefreshTime);
+
+function ClickBody() {
+	SoundObject.muted = false;
+}
+document.body.addEventListener("click", ClickBody);
+
