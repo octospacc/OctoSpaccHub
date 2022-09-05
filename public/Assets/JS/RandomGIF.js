d80e6a58ada5586edd85550ccc74e6d87972b7bf
octospacc <octo@tutamail.com>
2022-09-05 16:29:51 +0200

    Updates

--- public/Assets/JS/RandomGIF.js
+++ public/Assets/JS/RandomGIF.js
@@ -1,6 +1,6 @@
 function RandomGIF() {
 	GifID = Math.floor((Math.random() * 25)/2);
-	GifURL = "/Web-ThirdParty-Unknown/Assets/Media/Backgrounds/" + GifID + ".gif";
+	GifURL = "https://octospacc.gitlab.io/Web-ThirdParty-Unknown/Assets/Media/Backgrounds/" + GifID + ".gif";
 	document.body.style.backgroundImage = "url(" + GifURL + ")";
 }
 
