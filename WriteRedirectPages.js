c60c72c61ec3526a353262bb6e9b667b5bf30b6e
octospacc <octo@tutamail.com>
2024-03-19 17:46:09 +0100

    Update FramesBrowser, add redirect pages script

---
+++ WriteRedirectPages.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+for (const page of [
+	{ path: "a/fb", target: "'../../FramesBrowser/'+location.hash"           }, // Apps/FramesBrowser
+	{ path: "s/dh", target: "'../../FramesBrowser/#_1|f=1|h='+location.hash" }, // Services/DataHTML
+]) {
+	const dirPath = `./${page.path}`;
+	if (!fs.existsSync(dirPath)){
+		fs.mkdirSync(dirPath, { recursive: true });
+	}
+	fs.writeFileSync(`${dirPath}/index.html`, `<!DOCTYPE html><html>
+<head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
+<body>
+<script>
+function r(){ location=${page.target} }
+r()
+</script>
+<p>Redirecting...</p>
+<button onclick="r()">Click if you are not automatically redirected</button>
+</body>
+</html>`);
+}
