00d13784c019561548db1a65c884b8373ff43d11
octospacc <octo@tutamail.com>
2026-03-03 01:40:41 +0100

    Update home page; Add new MdAdmin WIP app

---
+++ source/MdAdmin/Build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo index.html \
+	node_modules/@materializecss/materialize/dist/css/materialize.min.css \
+	node_modules/@materializecss/materialize/dist/js/materialize.min.js \
+	node_modules/toml-js/lib/toml.min.js \
+	node_modules/marked/lib/marked.esm.js \
+	node_modules/webdav/dist/web/index.js \
+;
